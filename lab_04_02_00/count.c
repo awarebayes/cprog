723575cb8c0a0efd2300ad50401b51c912f507ca
Mikhail S <awarebayes@gmail.com>
2021-05-01 14:18:35 +0300

    lab 2 written

---
+++ lab_04_02_00/count.c
@@ -0,0 +1,22 @@
+void print_word_count(char **pa, int n_words)
+{
+    int count = 0;
+    int encountered = 0;
+
+    for (int i = 0; i < n_words; i++)
+    {
+        encountered = 0;
+        for (int j = 0; j < i; j++)
+            if (strcmp(pa[i], pa[j]) == 0)
+                encountered = 1;
+
+        if (!encountered)
+        {
+            count = 0;
+            for (int j = i; j < n_words; j++)
+                if (strcmp(pa[i], pa[j]) == 0)
+                    count++;
+            printf("Result: %s %d\n", pa[i], count);
+        }
+    }
+}
--- lab_04_02_00/main.c
+++ lab_04_02_00/main.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 #include <string.h>
+#include "split.c"
+#include "count.c"
+
+#define MSLEN 256 // MaxStringLen
+#define MWORDS 128 // MaxWords in string
+#define MWORDLEN 16 // MaxWordLen
+
+int read_line(char *s, int n);
+void print_error(const int ec);
+
+enum ec
+{
+    ok,
+    no_words,
+    input_error
+};
+
+int main()
+{
+    int ec = ok;
+    char line[MSLEN];
+    int line_len;
+
+    line_len = read_line(line, MSLEN);
+    char words[MWORDS][MWORDLEN+1];
+    char *pa[MWORDLEN+1];
+
+    transform(MWORDS, MWORDLEN+1, *words, pa);
+    int n_words;
+
+    n_words = split(pa, line, line_len);
+    if (n_words == 0 || line_len == 0)
+        ec = no_words;
+    else
+        print_word_count(pa, n_words);
+    print_error(ec);
+    return ec;
+}
 
 int read_line(char *s, int n)
 {
@@ -10,3 +48,18 @@ int read_line(char *s, int n)
     s[i] = '\0';
     return i;
 }
+
+void print_error(const int ec)
+{
+    switch (ec)
+    {
+        case ok:
+            break;
+        case no_words:
+            printf("Error: no words in line\n");
+            break;
+        case input_error:
+            break;
+    }
+}
+
