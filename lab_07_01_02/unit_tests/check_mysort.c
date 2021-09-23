d5a8a7554f233b28df1dc2188b9521c4a740762c
Mikhails S <awarebayes@gmail.com>
2021-09-23 15:11:35 +0300

    tests fixed

--- lab_07_01_02/unit_tests/check_mysort.c
+++ lab_07_01_02/unit_tests/check_mysort.c
@@ -8,7 +8,7 @@ START_TEST(test_mysort_1)
     int n = sizeof(a) / sizeof(int);
 
     mysort(a, n, sizeof(int), int_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
@@ -19,18 +19,18 @@ START_TEST(test_mysort_2)
     int n = sizeof(a) / sizeof(int);
 
     mysort(a, n, sizeof(int), int_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
 START_TEST(test_mysort_3)
 {
     int a[] = { 3, -2, 1 };
-    int b[] = { -2, 3, 3 };
+    int b[] = { -2, 1, 3 };
     int n = sizeof(a) / sizeof(int);
 
     mysort(a, n, sizeof(int), int_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
@@ -38,10 +38,9 @@ START_TEST(test_mysort_4)
 {
     int a[] = {-2, 3, 1};
     int b[] = {-2, 3, 1};
-    int n = sizeof(a) / sizeof(int);
 
     mysort(a, 0, sizeof(int), int_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
@@ -49,10 +48,9 @@ START_TEST(test_mysort_5)
 {
     int a[] = { 3, 2, 1, -100 };
     int b[] = { 1, 2, 3, -100 };
-    int n = sizeof(a) / sizeof(int);
 
     mysort(a, 3, sizeof(int), int_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
@@ -64,11 +62,10 @@ int double_cmp(const void * a, const void * b)
 START_TEST(test_mysort_6)
 {
     double a[] = { 3.0, 2.0, 1.0, -100.0 };
-    double b[] = { -100.0, 1.0, 2.0, 3.0 };
-    int n = sizeof(a) / sizeof(double);
+    double b[] = { 1.0, 2.0, 3.0, -100.0 };
 
     mysort(a, 3, sizeof(double), double_cmp);
-    ck_assert_mem_eq(a, b, n);
+    ck_assert_mem_eq(a, b, sizeof(a));
 }
 END_TEST
 
