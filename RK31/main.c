2997480eff1ae5f3fac9b2c9657ee929d4050008
iaa20u494 <ilinandrey963@yandex.ru>
2021-10-25 11:03:42 +0300

    RK written

---
+++ RK31/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "matrix.h"
+
+int main(int argc, char **argv)
+{
+	FILE *fin = fopen(argv[1], "r");
+	int n_iter = atoi(argv[2]);
+	int ended = 0;
+	matrix_t old_state = matrix_read(fin);
+	matrix_t new_state = old_state;
+	printf("0\n");
+	matrix_print(&old_state, stdout);
+	int i = 0;
+	for (i = 1; ((i <= n_iter) || n_iter < 0) && !ended ; i++)
+	{
+		printf("Step: %d\n", i);
+		new_state = life_update(&old_state, &ended);
+		matrix_print(&new_state, stdout);
+		matrix_delete(&old_state);
+		old_state = new_state;
+	}
+	printf("i: %d %d\n", n_iter, i);
+	if (ended)
+		printf("Ended! :(\n");
+	
+	matrix_delete(&new_state);
+	fclose(fin);
+}
