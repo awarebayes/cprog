a90032e2c445dd8f7909f3bbf82f7dcb9b533ae4
Mikhails S <awarebayes@gmail.com>
2021-11-15 16:51:43 +0300

    deleted folder fixed

--- lab_10_02_01/src/main.c
+++ lab_10_02_01/src/main.c
@@ -13,22 +13,18 @@ int main()
 {
 	int ec = 0;
 	char command[BUFFER_SIZE];
-	while (!ec)
-	{
-		printf("in>\n");
-		fgets(command, BUFFER_SIZE, stdin);
-		command[strcspn(command, "\n")] = 0;
-		if (strcmp(command, "val") == 0)
-			val(&ec);
-		else if (strcmp(command, "ddx") == 0)
-			ddx(&ec);
-		else if (strcmp(command, "sum") == 0)
-			sum(&ec);
-		else if (strcmp(command, "dvd") == 0)
-			dvd(&ec);
-		else
-			ec = bad_command_err;
-		printf("----\n");
-	}
+	printf("in>\n");
+	fgets(command, BUFFER_SIZE, stdin);
+	command[strcspn(command, "\n")] = 0;
+	if (strcmp(command, "val") == 0)
+		val(&ec);
+	else if (strcmp(command, "ddx") == 0)
+		ddx(&ec);
+	else if (strcmp(command, "sum") == 0)
+		sum(&ec);
+	else if (strcmp(command, "dvd") == 0)
+		dvd(&ec);
+	else
+		ec = bad_command_err;
 	return ec;
 }
